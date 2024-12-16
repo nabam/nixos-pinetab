3607fd8a7cbc091d48f5d7d96e0e83c19c476a5b
Leo Popov <leo@nabam.net>
2024-12-16 21:16:57 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "Build NixOS images for PineTab";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rockchip = {
+      url = "github:nabam/nixos-rockchip";
+    };
+  };
+
+  # Use cache with packages from nabam/nixos-rockchip CI.
+  nixConfig = {
+    extra-substituters = [ "https://nabam-nixos-rockchip.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM"
+    ];
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      devices = buildPlatform: {
+        "PineTab2" = {
+          # Use cross-compilation for uBoot and Kernel.
+          uBoot = inputs.rockchip.packages.${buildPlatform}.uBootPineTab2;
+          kernel = inputs.rockchip.legacyPackages.${buildPlatform}.kernel_linux_6_12_pinetab;
+          firmware = [ inputs.rockchip.packages.${buildPlatform}.bes2600 ];
+
+          extraModules = [
+            ./pinetab2-gnome.nix
+            inputs.rockchip.nixosModules.dtOverlayPCIeFix
+            inputs.rockchip.nixosModules.noZFS
+          ];
+        };
+      };
+
+      osConfigs =
+        buildPlatform:
+        builtins.mapAttrs (
+          name: value:
+          inputs.nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            modules = [
+              inputs.rockchip.nixosModules.sdImageRockchip
+              {
+                rockchip.uBoot = value.uBoot;
+                boot.kernelPackages = value.kernel;
+                hardware.firmware = value.firmware;
+              }
+            ] ++ value.extraModules;
+          }
+        ) (devices buildPlatform);
+
+      images =
+        buildPlatform:
+        builtins.mapAttrs (name: value: value.config.system.build.sdImage) (osConfigs buildPlatform);
+    in
+    {
+      # Set buildPlatform to "x86_64-linux" to benefit from cross-compiled packages in the cache.
+      nixosConfigurations = osConfigs "x86_64-linux";
+    }
+    // inputs.utils.lib.eachDefaultSystem (system: {
+      # Set buildPlatform to "x86_64-linux" to benefit from cross-compiled packages in the cache.
+      packages = {
+        default = self.packages.${system}.pinetab2-gnome;
+      } // images "x86_64-linux";
+
+      formatter = (import inputs.nixpkgs { inherit system; }).nixfmt-rfc-style;
+    });
+}
---
+++ pinetab2-gnome.nix
@@ -0,0 +1,96 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  system.stateVersion = "24.11";
+
+  networking.hostName = "pinetab2-gnome";
+
+  users.users.pinetab2 = {
+    initialPassword = "changeme";
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+  };
+
+  boot.kernelParams = [
+    "console=ttyS2,1500000n8"
+    "rootwait"
+    "root=LABEL=NIXOS_SD"
+    "rw"
+  ];
+
+  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.grub.enable = false;
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "bes2600-firmware"
+      "bes2600-firmware-aarch64-unknown-linux-gnu"
+    ];
+
+  zramSwap.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager.gnome.enable = true;
+      displayManager.gdm.enable = true;
+    };
+
+    automatic-timezoned.enable = true;
+    geoclue2.enableDemoAgent = lib.mkForce true;
+
+    flatpak.enable = true;
+    printing.enable = true;
+
+    avahi = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  security.sudo.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    cachix
+    firefox
+    gnomeExtensions.arc-menu
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.gjs-osk
+    gnomeExtensions.one-window-wonderland
+    htop
+  ];
+
+  environment.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+}
